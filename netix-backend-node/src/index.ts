60fb93a9dd3057e47b9d1f2f6cc62b15f15b2043
winqus <67459222+winqus@users.noreply.github.com>
2024-03-31 21:27:11 +0300

    Add linting config

--- netix-backend-node/src/index.ts
+++ netix-backend-node/src/index.ts
@@ -1,16 +1,15 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
 
 dotenv.config();
 
-const envFound = dotenv.config();
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const app = express();
 const port = process.env.PORT || 3055;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
