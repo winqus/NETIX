af25eb6f5f6cb9aa7e81fc4fb6a593b252f52ed8
winqus <67459222+winqus@users.noreply.github.com>
2024-04-01 21:37:47 +0300

    Add and setup body-parsing and celebrate, response error handling, separated route adding structure

--- netix-backend-node/src/index.ts
+++ netix-backend-node/src/index.ts
@@ -1,20 +1,49 @@
-import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { Joi, Segments, celebrate } from 'celebrate';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
+import config from '../config';
+import celebrateErrorHandler from './api/middlewares/celebrateErrorHandler';
+import routes from './api/routes';
 import { morganMiddleware, wLoggerInstance } from './loaders/logger';
-
-dotenv.config();
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+import ErrorResponse from './models/errorResponse.model';
 
 const app = express();
-const port = process.env.PORT || 3055;
 const logger = wLoggerInstance;
 
+app.get('/status', (_, res) => {
+  res.status(200).end();
+});
+
+app.head('/status', (_, res) => {
+  res.status(200).end();
+});
+
+// app.enable('trust proxy');
+
+app.use(cors());
+
 app.use(morganMiddleware);
+
+app.use(bodyParser.json());
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+app.post(
+  '/test',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      title: Joi.string().required(),
+      message: Joi.string().required(),
+    }),
+  }),
+  (req: Request, res: Response) => {
+    const { message } = req.body;
+    res.send({ message });
+  }
+);
 
 app.get('/logger', (_req: Request, res: Response) => {
   logger.error('This is an error log');
@@ -26,6 +55,18 @@ app.get('/logger', (_req: Request, res: Response) => {
   res.send('Check the logs');
 });
 
-app.listen(port, () => {
-  logger.debug(`[server]: Server is running at http://localhost:${port}`);
+app.use(config.api.prefix, routes());
+
+app.use(celebrateErrorHandler);
+
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  logger.error(`[server]: Unexpected error: ${err.message}`);
+
+  res.status(err.status || 500);
+  const response: ErrorResponse = { message: err.message || 'Unexpected error has occured.' };
+  res.json(response);
+});
+
+app.listen(config.port, () => {
+  logger.debug(`[server]: Server is running at http://localhost:${config.port} in ${config.environment} mode.`);
 });
