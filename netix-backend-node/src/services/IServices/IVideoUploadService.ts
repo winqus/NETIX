865d1b1d3b1b31b8153615abcc3b9fa6e631fd8b
winqus <67459222+winqus@users.noreply.github.com>
2024-05-01 18:11:30 +0300

    Delete deprecated videoRoute, controllers, mappers, schemas, services, repos #7

--- netix-backend-node/src/services/IServices/IVideoUploadService.ts
+++
@@ -1,6 +0,0 @@
-import { Result } from '../../core/logic/Result';
-import { VideoChunkUploadDTO } from '../../dto/videoUploadDTOs';
-
-export default interface IVideoUploadService {
-  saveVideoChunkFile(videoChunkUpload: VideoChunkUploadDTO, overwrite?: boolean): Promise<Result<void>>;
-}
