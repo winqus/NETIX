3a3ee2cb02cd2dc10f98fbc5e8dc1c10aacd4ff6
winqus <67459222+winqus@users.noreply.github.com>
2024-05-03 03:06:41 +0300

    Add FE 0Auth implementations #19

--- frontend-web-angular/src/app/components/navbar/navbar.component.ts
+++ frontend-web-angular/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { SvgIconsComponent } from '../svg-icons/svg-icons.component';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule } from '@angular/common';
 
 interface NavbarProps {
   title: string;
@@ -9,15 +11,22 @@ interface NavbarProps {
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, SvgIconsComponent],
+  imports: [RouterModule, SvgIconsComponent, CommonModule],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
   @Input() props: NavbarProps = { title: '' };
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    public auth: AuthService
+  ) {}
 
   get isUploadRoute(): boolean {
     return this.router.url === '/upload';
+  }
+
+  logout(): void {
+    this.auth.logout();
   }
 }
