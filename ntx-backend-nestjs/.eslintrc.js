7662255260d481dc4375c896b3c3e2d7f015bec8
winqus <67459222+winqus@users.noreply.github.com>
2024-07-03 22:34:07 +0300

    Add linting rules, docker-compose file, formatting settings #25

--- ntx-backend-nestjs/.eslintrc.js
+++ ntx-backend-nestjs/.eslintrc.js
@@ -21,5 +21,32 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/explicit-member-accessibility": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-parameter-properties": 0,
+    "@typescript-eslint/interface-name-prefix": 0,
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true
+      }
+    ],
+    "padding-line-between-statements": ["error", { blankLine: "always", prev: "*", next: "return" }],
+    "dot-location": ["error", "property"],
+    "lines-between-class-members": [
+      "error",
+      {
+        enforce: [{ blankLine: "always", prev: "method", next: "method" }]
+      }
+    ],
+    "arrow-parens": ["error", "always"],
+    "curly": ["error", "all"]
   },
 };
