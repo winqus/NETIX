f94a338e551c51eda17ef19d7c3a2413a3e32aa9
Gediminas Šeibokas <65848857+Stebis-dev@users.noreply.github.com>
2024-09-19 20:05:41 +0300

    Create movie e2e test #57

---
+++ ntx-web-fe-angular/cypress/e2e/create-movie.cy.ts
@@ -0,0 +1,38 @@
+import { makeRandomMovieRuntime, makeRandomMovieSummary, makeRandomMovieTitle } from 'cypress/support/randomDataFactory';
+
+// cy.intercept -- waits for back end response
+
+describe('create movie', () => {
+  it('shoudl navigate to create title page', () => {
+    cy.visit('/createTitle');
+    cy.url().should('include', '/createTitle');
+    cy.contains('Title');
+    cy.get('.btn').contains('CREATE').should('be.disabled');
+  });
+
+  it('should allow creating with valid fields', () => {
+    cy.visit('/createTitle');
+
+    cy.get('#title').type(makeRandomMovieTitle());
+    cy.get('#summary').type(makeRandomMovieSummary());
+    cy.get('#originallyReleasedAt').type('2002-09-11');
+    cy.get('#runtimeMinutes').type(makeRandomMovieRuntime().toString());
+
+    cy.get('.relative > .w-full').selectFile('cypress/files/1_sm_284x190.webp');
+
+    cy.get('.btn').contains('CREATE');
+  });
+
+  it('should create a movie', () => {
+    cy.visit('/createTitle');
+
+    cy.get('#title').type(makeRandomMovieTitle());
+    cy.get('#summary').type(makeRandomMovieSummary());
+    cy.get('#originallyReleasedAt').type('2002-09-11');
+    cy.get('#runtimeMinutes').type(makeRandomMovieRuntime().toString());
+
+    cy.get('.relative > .w-full').selectFile('cypress/files/1_sm_284x190.webp');
+
+    cy.get('.btn').contains('CREATE').click();
+  });
+});
