ce8992f05b99565b699e597171fe7ef60a7f7773
Gediminas Šeibokas <65848857+Stebis-dev@users.noreply.github.com>
2024-09-19 20:13:30 +0300

    Add route path for testing #53

--- ntx-web-fe-angular/cypress/e2e/create-movie.cy.ts
+++ ntx-web-fe-angular/cypress/e2e/create-movie.cy.ts
@@ -1,8 +1,14 @@
+import { getUploadMovieUrl } from '@ntx/app/shared/config/api-endpoints';
+import convertRouteToPath from 'cypress/support/convertRoute';
 import { makeRandomMovieRuntime, makeRandomMovieSummary, makeRandomMovieTitle } from 'cypress/support/randomDataFactory';
 
 // cy.intercept -- waits for back end response
 
 describe('create movie', () => {
+  beforeEach(() => {
+    cy.intercept(convertRouteToPath(getUploadMovieUrl())).as('BE_CreateMovie');
+  });
+
   it('shoudl navigate to create title page', () => {
     cy.visit('/createTitle');
     cy.url().should('include', '/createTitle');
@@ -34,5 +40,6 @@ describe('create movie', () => {
     cy.get('.relative > .w-full').selectFile('cypress/files/1_sm_284x190.webp');
 
     cy.get('.btn').contains('CREATE').click();
+    cy.wait('@BE_CreateMovie').its('response.statusCode').should('eq', 201);
   });
 });
