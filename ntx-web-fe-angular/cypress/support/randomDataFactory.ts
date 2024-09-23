e5966a84970877d275b90fdcfdf4c9af69a6b7c7
Gediminas Šeibokas <65848857+Stebis-dev@users.noreply.github.com>
2024-09-23 20:15:34 +0300

    Update randomDataFactory #53

--- ntx-web-fe-angular/cypress/support/randomDataFactory.ts
+++ ntx-web-fe-angular/cypress/support/randomDataFactory.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 
-export const makeRandomMovieTitle = () => 'T_' + faker.number.int();
+export const makeRandomMovieTitle = () => faker.commerce.productDescription();
 export const makeLongRandomMovieTitle = () => 'T_' + faker.string.alpha({ length: 200 });
 export const makeRandomMovieSummary = () => faker.lorem.sentences(2);
 export const makeRandomMovieReleaseDate = () => {
@@ -11,17 +11,3 @@ export const makeRandomMovieReleaseDate = () => {
   return `${year}-${month}-${day}`;
 };
 export const makeRandomMovieRuntime = () => faker.number.int({ min: 1, max: 12000 });
-
-// example
-// export const randomBuildingCode = () => 'b' + Math.random().toString(36).substring(2, 6).toUpperCase();
-// export const randomFloorNumber = () => Math.floor(Math.random() * 1000);
-// export const randomRoomName = () => 'room' + Math.random().toString(36).substring(2, 6).toUpperCase();
-// export const randomName = () => 'name' + Math.random().toString(36).substring(2, 15).toUpperCase();
-// export const randomSerialNumber = () => 'serial' + Math.random().toString(36).substring(2, 20).toUpperCase();
-// export const randomEmail = () => 'email' + Math.random().toString(36).substring(2, 20) + '@isep.ipp.pt';
-// export const randomRobotTypeName = () => 'type' + Math.random().toString(36).substring(2, 6).toUpperCase();
-// export const randomRobotTypeBrand = () => 'brand' + Math.random().toString(36).substring(2, 6).toUpperCase();
-// export const randomRobotTypeModel = () => 'model' + Math.random().toString(36).substring(2, 6).toUpperCase();
-// export const randomRobotCode = () => 'c' + Math.random().toString(36).substring(2, 6).toUpperCase();
-// export const randomRobotNickname = () => 'r' + Math.random().toString(36).substring(2, 6).toUpperCase();
-// export const randomRobotSerialNumber = () => 's' + Math.random().toString(36).substring(2, 10).toUpperCase();
