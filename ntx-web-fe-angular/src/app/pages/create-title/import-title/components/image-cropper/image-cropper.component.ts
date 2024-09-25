f24d840c2505d8b558aca7fab27d40bf0ab5b4f4
JorisS <joris.saulius@gmail.com>
2024-09-25 17:50:39 +0300

    Add initial search component, for import tab #80

---
+++ ntx-web-fe-angular/src/app/pages/create-title/import-title/components/image-cropper/image-cropper.component.ts
@@ -0,0 +1,57 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, ViewChild } from '@angular/core';
+import { CropperComponent, AngularCropperjsModule } from 'angular-cropperjs';
+import { ImageService } from '@ntx-shared/services/image.service';
+import { MediaConstants } from '@ntx/app/shared/config/constants';
+
+export interface InputProps {
+  imageUrl?: string;
+}
+
+@Component({
+  selector: 'app-image-cropper',
+  standalone: true,
+  imports: [AngularCropperjsModule],
+  templateUrl: './image-cropper.component.html',
+})
+export class ImageCropperComponent implements OnInit, OnChanges {
+  @Input() props: InputProps = {};
+  @Output() cropped = new EventEmitter<Blob>();
+
+  @ViewChild('angularCropper')
+  public angularCropper: CropperComponent = new CropperComponent();
+
+  cropper: Cropper | undefined;
+  cropperOptions: Cropper.Options;
+  imageUrl: string | null = null;
+
+  defaultProps: InputProps = {
+    imageUrl: '',
+  };
+
+  constructor(public imageCropService: ImageService) {
+    this.cropperOptions = this.imageCropService.getCropperConfig();
+  }
+
+  ngOnInit() {
+    if (this.props.imageUrl) {
+      this.imageUrl = this.props.imageUrl;
+    }
+  }
+
+  ngOnChanges(): void {
+    this.props = { ...this.defaultProps, ...this.props };
+  }
+
+  cropImg() {
+    const canvasWidth = 500;
+    const canvasHeight = 500;
+
+    if (this.angularCropper.cropper) {
+      this.angularCropper.cropper.getCroppedCanvas({ width: canvasWidth, height: canvasHeight }).toBlob((blob: Blob | null) => {
+        if (blob) {
+          this.cropped.emit(blob);
+        }
+      }, MediaConstants.image.exportFormat);
+    }
+  }
+}
