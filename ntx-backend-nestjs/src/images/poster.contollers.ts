b99b8d70e8ff75f1368812a4a2f5866430eda9d2
winqus <67459222+winqus@users.noreply.github.com>
2024-10-03 18:57:35 +0300

    Add images-proxy controller, update swagger tags for posters ctrl #111

--- ntx-backend-nestjs/src/images/poster.contollers.ts
+++ ntx-backend-nestjs/src/images/poster.contollers.ts
@@ -13,19 +13,19 @@ import { ApiTags } from '@nestjs/swagger';
 import { CustomHttpInternalErrorException } from '@ntx/common/exceptions/HttpInternalError.exception';
 import { SimpleValidationPipe } from '@ntx/common/pipes/simple-validation.pipe';
 import {
+  IMAGES_SWAGGER_TAG,
   POSTER_CONTROLLER_BASE_PATH,
   POSTER_CONTROLLER_VERSION,
   POSTER_EXTENTION,
   POSTER_MIME_TYPE,
   POSTER_NO_ID_PROVIDED_ERROR,
-  POSTER_SWAGGER_TAG,
 } from './images.constants';
 import { PosterSize } from './images.types';
 import { PosterService } from './poster.service';
 import { ApiDocsForGetPoster } from './swagger/api-docs.decorators';
 import { makePosterFileName } from './utils/images.utils';
 
-@ApiTags(POSTER_SWAGGER_TAG)
+@ApiTags(IMAGES_SWAGGER_TAG)
 @Controller({
   path: POSTER_CONTROLLER_BASE_PATH,
   version: POSTER_CONTROLLER_VERSION,
