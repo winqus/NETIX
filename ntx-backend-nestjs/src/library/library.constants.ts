b441616be465b9ecd1abdf335312efe5bc91625f
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-10-05 10:12:19 +0300

    Updated library module constants #89

--- ntx-backend-nestjs/src/library/library.constants.ts
+++ ntx-backend-nestjs/src/library/library.constants.ts
@@ -1,9 +1,9 @@
 export const CONTROLLER_BASE_PATH = 'api/v1/library';
 export const CONTROLLER_VERSION = '1';
 
-export const ExternalProviders = {
-  NTX: 'ntx',
-  TMDB: 'TMDB',
-};
+export enum Providers {
+  NTX = 'NTX',
+  NTX_DISCOVERY = 'NTX_DISCOVERY',
+}
 
 export const DEFAULT_SEARCH_LIMIT = 10;
