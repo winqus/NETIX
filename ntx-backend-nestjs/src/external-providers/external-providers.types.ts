e31b27d09b847c5df4dc04e839152d5165f9b822
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-10-05 12:28:31 +0300

    Remove runtimeMinutes from ExternalTitleMetadata #89

--- ntx-backend-nestjs/src/external-providers/external-providers.types.ts
+++ ntx-backend-nestjs/src/external-providers/external-providers.types.ts
@@ -9,7 +9,6 @@ export interface ExternalTitle<T extends TitleType = TitleType> {
 }
 
 export interface ExternalTitleMetadata {
-  runtimeMinutes: number;
   name: string;
   originalName: string;
   summary: string;
