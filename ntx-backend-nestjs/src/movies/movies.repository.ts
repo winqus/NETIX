5417a74dfa91f59d1467d82fc4c740a3b99e4ef7
Gytismar <25962211+Gytismar@users.noreply.github.com>
2024-10-05 10:22:30 +0300

    Update library repository mapMovieDocumentToMovie and findAllByName

--- ntx-backend-nestjs/src/movies/movies.repository.ts
+++ ntx-backend-nestjs/src/movies/movies.repository.ts
@@ -11,12 +11,16 @@ export class MoviesRepository extends EntityRepository<Movie> {
     super(model);
   }
 
-  public async findAllByName(name: string): Promise<MovieDocument[]> {
+  public async findAllByName(name: string): Promise<Movie[]> {
     const query: FilterQuery<MovieDocument> = {
       name: { $regex: new RegExp(name, 'i') },
     };
 
-    return this.model.find(query).exec();
+    const movieDocuments = await this.model.find(query).exec();
+
+    const movies: Movie[] = movieDocuments.map(this.mapMovieDocumentToMovie);
+
+    return movies;
   }
 
   public async existsByID(uuid: string): Promise<boolean> {
@@ -37,5 +41,21 @@ export class MoviesRepository extends EntityRepository<Movie> {
     const count = await this.model.findOne(query);
 
     return count != null;
+  }
+
+  private mapMovieDocumentToMovie(document: MovieDocument): Movie {
+    return {
+      uuid: document.uuid,
+      createdAt: document.createdAt,
+      updatedAt: document.updatedAt,
+      posterID: document.posterID,
+      name: document.name,
+      type: document.type,
+      hash: document.hash,
+      originallyReleasedAt: document.originallyReleasedAt,
+      summary: document.summary,
+      runtimeMinutes: document.runtimeMinutes,
+      videoID: document.videoID,
+    };
   }
 }
