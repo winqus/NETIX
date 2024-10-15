fec17bdca470bd62daeec4eee7190cc9ebe60dff
Gediminas Šeibokas <65848857+Stebis-dev@users.noreply.github.com>
2024-10-15 22:57:49 +0300

    Add unit and e2e test #169

--- ntx-web-fe-angular/cypress/e2e/view-movies.cy.ts
+++ ntx-web-fe-angular/cypress/e2e/view-movies.cy.ts
@@ -1,10 +1,14 @@
 import { getMovieUrl } from '@ntx/app/shared/config/api-endpoints';
 import convertRouteToPath from 'cypress/support/convertRoute';
+import { makeRandomMovieName } from 'cypress/support/randomDataFactory';
 import { yearOnlyRegex } from 'cypress/support/regex';
 
 describe('view movies list', () => {
+  const movieName = makeRandomMovieName();
+  const SEARCH_RESULTS_ELEMENT = 'ul';
+  const SEARCH_RESULTS_ITEM_ELEMENT = 'li';
   before(() => {
-    cy.createMovieWithPoster();
+    cy.createMovieWithPoster(movieName);
     cy.createMovieWithPoster();
   });
 
@@ -23,10 +27,25 @@ describe('view movies list', () => {
     });
   });
 
-  it('should navigate to a movie details page', () => {
+  it('should searched movie navigate to a movie inspect page', () => {
     cy.visit('/');
     cy.wait('@BE_GetMovies');
     cy.get('app-movie-card').first().click();
+    cy.url().should('include', '/inspect/movies/MT-');
+  });
+
+  it('should navigate to movie inspect page', () => {
+    cy.visit('/');
+
+    cy.get('#searchBar').type(movieName);
+    cy.get(SEARCH_RESULTS_ELEMENT)
+      .should('be.visible')
+      .within(() => {
+        cy.get(SEARCH_RESULTS_ITEM_ELEMENT).should('have.length.greaterThan', 0);
+        cy.get(SEARCH_RESULTS_ITEM_ELEMENT).first().get('img').should('be.visible');
+        cy.get(SEARCH_RESULTS_ITEM_ELEMENT).first().contains('MOVIE');
+        cy.get(SEARCH_RESULTS_ITEM_ELEMENT).first().contains(movieName).click();
+      });
     cy.url().should('include', '/inspect/movies/MT-');
   });
 });
