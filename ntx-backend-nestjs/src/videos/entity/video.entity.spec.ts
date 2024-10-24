9b7b2d623bdbfa5ce722824d0823555c7872fb38
winqus <67459222+winqus@users.noreply.github.com>
2024-10-24 18:32:33 +0300

    Add empty test for tests to pass #196

--- ntx-backend-nestjs/src/videos/entity/video.entity.spec.ts
+++ ntx-backend-nestjs/src/videos/entity/video.entity.spec.ts
@@ -1,1 +1,8 @@
 // TODO: test creation of video entity (use movie entity spec as example)
+describe('Video', () => {
+  describe('create', () => {
+    it('should create a valid object with valid props', async () => {
+      // TODO: implement test
+    });
+  });
+});
