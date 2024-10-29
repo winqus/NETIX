cb1c7c0bfd3fa4c98ad01517c749b70074936056
Gediminas Šeibokas <65848857+Stebis-dev@users.noreply.github.com>
2024-10-29 13:05:10 +0200

    Cleaned movie card component #194

--- ntx-web-fe-angular/src/app/pages/movie-list/movie-card/movie-card.component.ts
+++ ntx-web-fe-angular/src/app/pages/movie-list/movie-card/movie-card.component.ts
@@ -5,6 +5,8 @@ import { MovieDTO } from '@ntx/app/shared/models/movie.dto';
 import { PosterSize } from '@ntx/app/shared/models/posterSize.enum';
 import { SvgIconsComponent } from '@ntx/app/shared/ui/svg-icons/svg-icons.component';
 
+export const titleUpdateBadgeThresholdDays = 7;
+
 @Component({
   selector: 'app-movie-card',
   standalone: true,
@@ -50,5 +52,37 @@ export class MovieCardComponent {
     if (event.key === 'Enter' || event.key === ' ') {
       this.navigateToMovie();
     }
+  }
+
+  isMovieRecentlyUpdated(): boolean {
+    if (this.movie == null) return false;
+
+    const currentDate = new Date();
+    const sevenDaysAgo = new Date(currentDate);
+    sevenDaysAgo.setDate(currentDate.getDate() - titleUpdateBadgeThresholdDays);
+
+    return this.movie?.updatedAt >= sevenDaysAgo && this.movie?.updatedAt <= currentDate;
+  }
+
+  updateRecentlyUpdatedLabel(): void {
+    this.recentlyUpdatedLabel = this.getRecentlyUpdatedLabel();
+  }
+
+  getRecentlyUpdatedLabel(): string {
+    if (!this.movie?.updatedAt) return '';
+
+    const currentDate = new Date();
+    const updatedDate = new Date(this.movie.updatedAt);
+    const timeDifference = currentDate.getTime() - updatedDate.getTime();
+
+    const minutesDifference = Math.floor(timeDifference / (1000 * 60));
+    const hoursDifference = Math.floor(minutesDifference / 60);
+    const daysDifference = Math.floor(hoursDifference / 24);
+
+    if (minutesDifference < 1) return `Updated just now`;
+    if (hoursDifference < 1) return `Updated ${minutesDifference} minutes ago`;
+    if (daysDifference < 1) return `Updated ${hoursDifference} hours ago`;
+
+    return `Updated ${daysDifference} days ago`;
   }
 }
