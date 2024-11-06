9d64adcf586e613c9838fed26cf3e42d667a36f7
winqus <67459222+winqus@users.noreply.github.com>
2024-11-06 13:30:36 +0200

    Add `/api/v1/videos/:id` endpoint by creating videos controller and updating videos service #210

--- ntx-backend-nestjs/src/videos/videos.mapper.ts
+++ ntx-backend-nestjs/src/videos/videos.mapper.ts
@@ -1,4 +1,5 @@
 import { createValidatedObject } from '@ntx/common/utils/class-validation.utils';
+import { VideoDTO } from './dto/video.dto';
 import { Video } from './entity/video.entity';
 
 export class VideosMapper {
@@ -16,5 +17,21 @@ export class VideosMapper {
 
   public static async any2Videos(any: any[]): Promise<Video[]> {
     return Promise.all(any.map((a) => VideosMapper.any2Video(a)));
+  }
+
+  public static async Video2VideoDTO(video: Video): Promise<VideoDTO> {
+    const videoDTO = createValidatedObject(VideoDTO, {
+      id: video.uuid,
+      createdAt: video.createdAt,
+      updatedAt: video.updatedAt,
+      name: video.name,
+      state: video.state,
+    });
+
+    return videoDTO;
+  }
+
+  public static async Videos2VideoDTOs(videos: Video[]): Promise<VideoDTO[]> {
+    return Promise.all(videos.map((v) => VideosMapper.Video2VideoDTO(v)));
   }
 }
--- ntx-backend-nestjs/src/videos/videos.service.ts
+++ ntx-backend-nestjs/src/videos/videos.service.ts
@@ -1,9 +1,16 @@
 import { InjectQueue } from '@nestjs/bullmq';
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { FileInStorage } from '@ntx/file-storage/types';
+import { VideoDTO } from './dto/video.dto';
 import { Video, VideoState } from './entity/video.entity';
 import { ProcessVideoJobPayload, ProcessVideoQueue } from './queues/process-video.types';
-import { PROCESS_VIDEO_JOBNAME, PROCESS_VIDEO_QUEUE } from './videos.constants';
+import {
+  PROCESS_VIDEO_JOBNAME,
+  PROCESS_VIDEO_QUEUE,
+  VIDEOS_ERROR_NO_ID_PROVIDED,
+  VIDEOS_ERROR_NOT_FOUND,
+} from './videos.constants';
+import { VideosMapper } from './videos.mapper';
 import { VideosRepository } from './videos.repository';
 
 @Injectable()
@@ -29,5 +36,24 @@ export class VideosService {
     await this.videoQueue.add(PROCESS_VIDEO_JOBNAME, payload);
 
     return this.videosRepository.createOne(newVideo);
+  }
+
+  public async findOne(id: string): Promise<VideoDTO> {
+    try {
+      if (id == null) {
+        throw new BadRequestException(VIDEOS_ERROR_NO_ID_PROVIDED);
+      }
+
+      const video = await this.videosRepository.findOneByUUID(id);
+
+      if (video == null) {
+        throw new NotFoundException(VIDEOS_ERROR_NOT_FOUND);
+      }
+
+      return VideosMapper.Video2VideoDTO(video);
+    } catch (error) {
+      this.logger.error(`Failed to find video with this ${id}: ${error.message}`);
+      throw error;
+    }
   }
 }
