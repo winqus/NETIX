37dd3655516e4f525f621d66887dd61242415c56
winqus <67459222+winqus@users.noreply.github.com>
2024-11-10 21:19:34 +0200

    Update again e2e spec TMDB rate limit, add logging to JestCacheFetch to attempt fix linux pipes #77

--- ntx-backend-nestjs/src/movies/movies.controller.e2e.spec.ts
+++ ntx-backend-nestjs/src/movies/movies.controller.e2e.spec.ts
@@ -49,7 +49,7 @@ describe('Movies API (e2e)', () => {
           TMDB: {
             enable: true,
             apiKey: 'x',
-            rateLimitMs: 0,
+            rateLimitMs: 1,
           },
         }),
         FileStorageModule.forRoot(storageType, options, true),
